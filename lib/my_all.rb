04af3cb76b76217620184172dc687edf963f900b
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 20:16:24 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ def my_all?(collection)
   i = 0
   return_values = []
   while i < collection.length
-      return_values = yield(collection[i])
+      return_values << yield(collection[i])
     i += 1
   end
 
